fe3e6c5a63d827326a8ad21efc5b2d236cf64647
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-27 19:35:36 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,35 @@
+import pytest
+import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="Выбор браузера: chrome или firefox"
+    )
+    parser.addoption(
+        "--headless",
+        action="store_true",
+        default=False,
+        help="Запуск в headless режиме"
+    )
+    parser.addoption(
+        "--url",
+        action="store",
+        default="https://stellarburgers.education-services.ru",
+        help="URL тестового приложения"
+    )
+
+
+@pytest.fixture(scope="function")
+def driver(request):
+    browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+    driver = None
