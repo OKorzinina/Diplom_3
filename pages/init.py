d138b669ec36d5e6094c65dbb1a5e9b0b38111f0
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-27 19:26:30 +0100

    Update init.py

--- pages/init.py
+++ pages/init.py
@@ -1,1 +1,6 @@
+from .base_page import BasePage
+from .auth_page import AuthPage
+from .main_page import MainPage
+from .order_page import OrderPage
 
+__all__ = ['BasePage', 'AuthPage', 'MainPage', 'OrderPage']
