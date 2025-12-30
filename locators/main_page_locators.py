c93d34b677e47bb0a7a86622801f1c708ebbcff0
Olga Korzinina <korzininao@gmail.com>
2025-12-30 21:47:30 +0100

    Обновление

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -1,9 +1,8 @@
-
 from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    # Навигация (используйте классы из вывода)
+    # Навигация 
     CONSTRUCTOR_BUTTON = (By.XPATH, "//a[contains(@class, 'AppHeader_header__link__3D_hX') and .//p[text()='Конструктор']]")
     ORDER_FEED_BUTTON = (By.XPATH, "//a[contains(@class, 'AppHeader_header__link__3D_hX') and .//p[text()='Лента Заказов']]")
     LOGO = (By.XPATH, "//div[contains(@class, 'AppHeader_header__logo')]")
@@ -14,7 +13,7 @@ class MainPageLocators:
     # Ингредиенты
     FLUORESCENT_BUN = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']")
     
-    # Локатор для счетчика - несколько вариантов
+    # Локатор для счетчика 
     INGREDIENT_COUNTER = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/../following-sibling::div[contains(@class, 'counter')]//p")
     INGREDIENT_COUNTER_ALT = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/../../div[contains(@class, 'counter')]")
     
@@ -34,7 +33,7 @@ class MainPageLocators:
     # Секция ингредиентов
     INGREDIENT_SECTION = (By.XPATH, "//section[contains(@class, 'BurgerIngredients_ingredients__1N8v2')]")
     
-    # Простые варианты для навигации
+    # Простые варианты для навигации 
     CONSTRUCTOR_BUTTON_SIMPLE = (By.XPATH, "//p[text()='Конструктор']/parent::a")
     ORDER_FEED_BUTTON_SIMPLE = (By.XPATH, "//p[text()='Лента Заказов']/parent::a")
     
@@ -50,3 +49,32 @@ class MainPageLocators:
     
     # Ингредиент-родитель для более точного поиска
     INGREDIENT_PARENT = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/..")
+    
+     
+    # Самый простой поиск по тексту (на случай если структура DOM изменилась)
+    CONSTRUCTOR_BUTTON_TEXT = (By.XPATH, "//*[text()='Конструктор']")
+    ORDER_FEED_BUTTON_TEXT = (By.XPATH, "//*[text()='Лента Заказов']")
+    
+    # Поиск по классам кнопок (если текст локализован или меняется)
+    CONSTRUCTOR_BUTTON_BY_CLASS = (By.XPATH, "//a[contains(@class, 'AppHeader_header__link__3D_hX')][1]")
+    ORDER_FEED_BUTTON_BY_CLASS = (By.XPATH, "//a[contains(@class, 'AppHeader_header__link__3D_hX')][2]")
+    
+        
+    # Проверка что страница загрузилась
+    PAGE_LOADED_INDICATOR = (By.XPATH, "//main[contains(@class, 'App')]")
+    
+    # Локатор для проверки видимости ингредиента (не только текста)
+    FLUORESCENT_BUN_CONTAINER = (By.XPATH, "//div[.//p[text()='Флюоресцентная булка R2-D3']]")
+    
+    # Локатор для всплывающего окна/подсказки счетчика
+    COUNTER_POPUP = (By.XPATH, "//div[contains(@class, 'counter__num')]")
+    
+     
+    # Любой элемент с классом "counter" рядом с ингредиентом
+    ANY_COUNTER_NEAR_INGREDIENT = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/ancestor::div[contains(@class, 'ingredient')]//div[contains(@class, 'counter')]")
+    
+    # Любое модальное окно (для fallback)
+    ANY_MODAL = (By.XPATH, "//div[contains(@class, 'Modal') or contains(@class, 'modal')]")
+    
+    # Любая кнопка закрытия
+    ANY_CLOSE_BUTTON = (By.XPATH, "//button[contains(@class, 'close') or contains(@class, 'Modal_close') or @aria-label='Закрыть']")
