8db32b7237b5409e4340eb7ac8239ce794e4d68d
Olga Korzinina <korzininao@gmail.com>
2026-01-04 20:00:59 +0100

    Обновление order_page

--- pages/order_page.py
+++ pages/order_page.py
@@ -6,23 +6,34 @@ from locators.order_page_locators import OrderPageLocators
 class OrderPage(BasePage):
     locators_class = OrderPageLocators
 
-    @allure.step("Проверить видимость карточек заказов")
-    def is_order_cards_visible(self):
-        return self.is_element_visible(OrderPageLocators.ORDER_CARDS, timeout=10)
+    @allure.step("Дождаться появления карточек заказов")
+    def wait_for_order_cards_visible(self):
+        """Ждем карточки заказов - тест упадет если не появятся"""
+        self.wait_for_visible(OrderPageLocators.ORDER_CARDS, timeout=10)
     
-    @allure.step("Кликнуть на первую карточку заказа")
-    def click_first_order_card(self):
+    @allure.step("Кликнуть на первую карточку заказа и дождаться модального окна")
+    def click_first_order_card_and_wait_modal(self):
+        """Линейный сценарий: клик → должно открыться модальное окно"""
         self.click(OrderPageLocators.ORDER_CARDS)
+        self.wait_for_visible(OrderPageLocators.ORDER_MODAL, timeout=5)
     
-    @allure.step("Проверить открытие модального окна заказа")
-    def is_order_modal_open(self):
-        return self.is_element_visible(OrderPageLocators.ORDER_MODAL, timeout=5)
+    @allure.step("Дождаться открытия модального окна заказа")
+    def wait_for_order_modal_open(self):
+        """Ждем открытия модального окна заказа - тест упадет если не откроется"""
+        self.wait_for_visible(OrderPageLocators.ORDER_MODAL, timeout=5)
+    
+    @allure.step("Дождаться закрытия модального окна заказа")
+    def wait_for_order_modal_close(self):
+        """Ждем закрытия модального окна заказа - тест упадет если не закроется"""
+        self.wait_for_invisible(OrderPageLocators.ORDER_MODAL, timeout=5)
     
     @allure.step("Закрыть модальное окно заказа")
-    def close_order_modal(self):
+    def close_order_modal_and_wait(self):
+        """Линейный сценарий: клик → должно закрыться"""
         self.click(OrderPageLocators.ORDER_MODAL_CLOSE)
         self.wait_for_invisible(OrderPageLocators.ORDER_MODAL, timeout=5)
     
-    @allure.step("Проверить видимость раздела 'В работе'")
-    def is_in_progress_section_visible(self):
-        return self.is_element_visible(OrderPageLocators.IN_PROGRESS_SECTION, timeout=10)
+    @allure.step("Дождаться появления раздела 'В работе'")
+    def wait_for_in_progress_section_visible(self):
+        """Ждем раздел 'В работе' - тест упадет если не появится"""
+        self.wait_for_visible(OrderPageLocators.IN_PROGRESS_SECTION, timeout=10)
