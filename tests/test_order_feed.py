c1c5fcaf99de5f54fc43e52107976d8d841fffe4
Olga Korzinina <korzininao@gmail.com>
2026-01-09 09:04:54 +0100

    Обновление теста фун

--- tests/test_order_feed.py
+++ tests/test_order_feed.py
@@ -8,7 +8,7 @@ class TestOrderFeed:
     
     @allure.title("Тест: Отображение ленты заказов")
     def test_order_feed_display(self, driver):
-        """Проверка отображения ленты заказов БЕЗ ВЕТВЛЕНИЙ"""
+        """Проверка отображения ленты заказов с проверками"""
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
 
@@ -18,12 +18,11 @@ class TestOrderFeed:
         main_page.click_order_feed()
         assert "feed" in driver.current_url, "Не перешли в ленту заказов"
         
-        # Метод wait_for_order_cards_visible сам упадет если карточек нет
         order_page.wait_for_order_cards_visible()
     
     @allure.title("Тест: Карточки заказов в ленте")
     def test_order_cards(self, driver):
-        """Проверка отображения карточек заказов БЕЗ ВЕТВЛЕНИЙ"""
+        """Проверка отображения карточек заказов с проверками"""
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
 
@@ -33,12 +32,11 @@ class TestOrderFeed:
         main_page.click_order_feed()
         assert "feed" in driver.current_url, "Не перешли в ленту заказов"
 
-        # Метод wait_for_order_cards_visible сам упадет если карточек нет
         order_page.wait_for_order_cards_visible()
     
     @allure.title("Тест: Открытие и закрытие модального окна заказа")
     def test_order_modal(self, driver):
-        """Проверка работы с модальным окном заказа БЕЗ ВЕТВЛЕНИЙ"""
+        """Проверка работы с модальным окном заказа с проверками"""
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
 
@@ -52,15 +50,19 @@ class TestOrderFeed:
         
         # Кликаем на первую карточку
         order_page.click_first_order_card_and_wait_modal()
-        # Метод click_first_order_card_and_wait_modal уже ждет открытия модалки
         
-        # Закрываем модальное окно
-        order_page.close_order_modal_and_wait()
-        # Метод close_order_modal_and_wait уже ждет закрытия модалки
+        # Проверяем что остались в ленте заказов
+        assert "feed" in driver.current_url, "Не находимся в ленте заказов после открытия модалки"
+        
+        # Закрываем модальное окно клавишей ESCAPE
+        main_page.send_keys_escape()
+        
+        # Проверяем что остались в ленте заказов после закрытия
+        assert "feed" in driver.current_url, "Не остались в ленте заказов после закрытия модалки"
     
     @allure.title("Тест: Раздел 'В работе'")
     def test_in_progress_section(self, driver):
-        """Проверка раздела 'В работе' БЕЗ ВЕТВЛЕНИЙ"""
+        """Проверка раздела 'В работе' с проверками"""
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
 
@@ -70,12 +72,11 @@ class TestOrderFeed:
         main_page.click_order_feed()
         assert "feed" in driver.current_url, "Не перешли в ленту заказов"
 
-        # Метод wait_for_in_progress_section_visible сам упадет если раздела нет
         order_page.wait_for_in_progress_section_visible()
     
     @allure.title("Тест: Обновление страницы")
     def test_page_refresh(self, driver):
-        """Проверка обновления страницы БЕЗ ВЕТВЛЕНИЙ"""
+        """Проверка обновления страницы с проверками"""
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
 
@@ -87,8 +88,14 @@ class TestOrderFeed:
         
         order_page.wait_for_order_cards_visible()
         
+        # Проверяем что находимся в ленте заказов до обновления
+        assert "feed" in driver.current_url, "Не находимся в ленте заказов до обновления"
+        
         # Обновляем страницу
         driver.refresh()
+        
+        # Проверяем что остались в ленте заказов после обновления
+        assert "feed" in driver.current_url, "Не остались в ленте заказов после обновления"
         
         # После обновления снова проверяем карточки
         order_page.wait_for_order_cards_visible()
