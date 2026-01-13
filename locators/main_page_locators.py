251d0b8360d1b0c988028be635ea6b4ed517500a
Olga Korzinina <korzininao@gmail.com>
2026-01-13 20:34:48 +0100

    Обновлен код

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -2,23 +2,19 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    # Кнопки навигации в шапке
-    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
-    ORDER_FEED_BUTTON = (By.XPATH, "//p[text()='Лента Заказов']")
-    
-    # Ингредиенты
-    FLUORESCENT_BUN = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']")
-    
-    # Зона для перетаскивания 
-    BUN_DROP_AREA = (By.XPATH, "//ul[contains(@class, 'BurgerConstructor_basket__list')]")
-    
-    # Счетчик ингредиента
-    INGREDIENT_COUNTER = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/ancestor::div[contains(@class, 'ingredient')]//p[contains(@class, 'counter')]")
-    
-    # Модальное окно ингредиента
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[contains(text(), 'Конструктор')]")
+    ORDER_FEED_BUTTON = (By.XPATH, "//p[contains(text(), 'Лента Заказов')]")
+    FLUORESCENT_BUN = (By.XPATH, "//img[@alt='Флюоресцентная булка R2-D3']")
+    INGREDIENT_COUNTER = (By.XPATH, "//p[contains(@class, 'counter')]")
     INGREDIENT_MODAL = (By.XPATH, "//div[contains(@class, 'Modal_modal')]")
-    MODAL_TITLE = (By.XPATH, "//div[contains(@class, 'Modal_header')]//h2")
-    # Кнопка закрытия - крестик в правом верхнем углу
-    MODAL_CLOSE_BUTTON = (By.XPATH, "//div[contains(@class, 'Modal_modal')]/button")
-    # Альтернативная кнопка - клик по оверлею (вне модального окна)
-    MODAL_CLOSE_BUTTON_ALT = (By.XPATH, "//div[contains(@class, 'Modal_overlay')]")
+    MODAL_TITLE = (By.XPATH, "//h2[contains(@class, 'Modal_title')]")
+    #MODAL_CLOSE_BUTTON = (By.XPATH, "//button[contains(@class, 'Modal_close')]")
+    MODAL_CLOSE_BUTTON = (By.XPATH, "//button[@type='button' and contains(@class, 'close')]")
+    # Локаторы для drag and drop
+    BUN_DROP_AREA = (By.XPATH, "//div[contains(@class, 'BurgerConstructor_basket')]")
+    CONSTRUCTOR_SECTION = (By.XPATH, "//section[contains(@class, 'BurgerIngredients')]")
+    
+    PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[contains(text(), 'Личный Кабинет')]")
+    EMAIL_FIELD = (By.XPATH, "//input[@name='name' and @type='text']")
+    PASSWORD_FIELD = (By.XPATH, "//input[@type='password']")
+    LOGIN_BUTTON = (By.XPATH, "//button[contains(text(), 'Войти')]")
