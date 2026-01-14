d79e2027ab201eb11aa9e936f2533208404c9775
Olga Korzinina <korzininao@gmail.com>
2026-01-14 22:10:17 +0100

    Обновление auth локаторf

--- locators/auth_page_locators.py
+++ locators/auth_page_locators.py
@@ -1,21 +1,17 @@
 from selenium.webdriver.common.by import By
 
-
 class AuthPageLocators:
-    # Поля ввода
-    EMAIL_INPUT = (By.XPATH, "//input[@name='email']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@name='password']")
-    
-    # Кнопки
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
-    REGISTER_BUTTON = (By.XPATH, "//a[text()='Зарегистрироваться']")
-    FORGOT_PASSWORD_BUTTON = (By.XPATH, "//a[text()='Восстановить пароль']")
-    
-    # Заголовок
-    LOGIN_TITLE = (By.XPATH, "//h2[text()='Вход']")
-    
-    # Навигация
-    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
-    LOGO = (By.XPATH, "//div[contains(@class, 'logo')]")
+    # Заголовок "Вход"
+    LOGIN_TITLE = (By.XPATH, ".//h2[text()='Вход']")
 
-    
+    # Поля ввода 
+    EMAIL_INPUT = (By.XPATH, ".//label[text()='Email']/following-sibling::input")
+    PASSWORD_INPUT = (By.XPATH, ".//label[text()='Пароль']/following-sibling::input")
+
+    # Кнопка Войти
+    LOGIN_BUTTON = (By.XPATH, ".//button[text()='Войти']")
+
+    # Кнопки-ссылки
+    REGISTER_BUTTON = (By.XPATH, ".//a[text()='Зарегистрироваться']")
+    FORGOT_PASSWORD_BUTTON = (By.XPATH, ".//a[text()='Восстановить пароль']")
+
