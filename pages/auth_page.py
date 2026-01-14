7f95efb2f8e04acf9cb5c3a6b87904a6e3bfb43f
Olga Korzinina <korzininao@gmail.com>
2026-01-14 22:18:49 +0100

    Обновление auth page

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,48 +1,39 @@
 import allure
-from .base_page import BasePage
+from pages.base_page import BasePage
 from locators.auth_page_locators import AuthPageLocators
 from urls import Urls
 
-
 class AuthPage(BasePage):
-    
+
     @allure.step("Открыть страницу авторизации")
     def open(self):
         self.driver.get(Urls.LOGIN_URL)
-        self.wait_for_visible(AuthPageLocators.HEADER)
-    
+        self.wait_for_visible(AuthPageLocators.LOGIN_TITLE)
+
     @allure.step("Ввести email: {email}")
     def enter_email(self, email):
         self.send_keys(AuthPageLocators.EMAIL_INPUT, email)
-    
+
     @allure.step("Ввести пароль: {password}")
     def enter_password(self, password):
         self.send_keys(AuthPageLocators.PASSWORD_INPUT, password)
-    
+
     @allure.step("Нажать кнопку 'Войти'")
     def click_login_button(self):
         self.click(AuthPageLocators.LOGIN_BUTTON)
-    
-    @allure.step("Выполнить авторизацию с email: {email} и паролем: {password}")
+
+    @allure.step("Выполнить авторизацию")
     def login(self, email, password):
+        self.wait_for_visible(AuthPageLocators.EMAIL_INPUT)
         self.enter_email(email)
         self.enter_password(password)
         self.click_login_button()
-    
-    @allure.step("Нажать 'Зарегистрироваться'")
+   
+
+    @allure.step("Нажать на ссылку 'Зарегистрироваться'")
     def click_register_link(self):
-        self.click(AuthPageLocators.REGISTER_LINK)
-    
-    @allure.step("Нажать 'Восстановить пароль'")
+        self.click(AuthPageLocators.REGISTER_BUTTON)
+
+    @allure.step("Нажать на ссылку 'Восстановить пароль'")
     def click_forgot_password_link(self):
         self.click(AuthPageLocators.FORGOT_PASSWORD_BUTTON)
-    
-    @allure.step("Проверить отображение ошибки")
-    def is_error_displayed(self):
-        return self.is_element_present(AuthPageLocators.ERROR_MESSAGE)
-    
-    @allure.step("Получить текст ошибки")
-    def get_error_text(self):
-        if self.is_error_displayed():
-            return self.get_text(AuthPageLocators.ERROR_MESSAGE)
-        return ""
