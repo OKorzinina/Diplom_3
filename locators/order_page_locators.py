21cfe32cdf4cd6e924d22b89c13ccc0a35596eb5
Olga Korzinina <korzininao@gmail.com>
2026-01-14 22:14:23 +0100

    Обновление order локатора

--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -1,24 +1,19 @@
 from selenium.webdriver.common.by import By
 
-
 class OrderPageLocators:
-    # Заголовок страницы
-    PAGE_HEADER = (By.XPATH, "//h1[text()='Лента заказов']")
-    
-    # Счетчики 
-    TOTAL_ORDERS_COUNTER = (By.XPATH, "//p[contains(@class, 'digits')][contains(text(), '2')]")  
-    TODAY_ORDERS_COUNTER = (By.XPATH, "//p[contains(@class, 'digits')][contains(text(), '3')]")  
-    
-    # Раздел "В работе" 
-    IN_PROGRESS_SECTION = (By.XPATH, "//*[contains(text(), 'В работе:')]")
-    
-    # Альтернативные локаторы для счетчиков
-    
-    ALL_DIGITS = (By.XPATH, "//p[contains(@class, 'digits')]")
-    
-    # Для теста "В работе" будем использовать родительский элемент
-    IN_PROGRESS_CONTAINER = (By.XPATH, "//*[contains(text(), 'В работе:')]/..")
+    # Весь список заказов (для проверки появления номера)
+    ORDER_LIST_NUMBERS = (By.XPATH, ".//li[contains(@class, 'OrderHistory_listItem')]//p[@class='text text_type_digits-default']")
 
-    # Доп локаторы:
-    IN_WORK_LIST = (By.XPATH, "//p[text()='В работе:']/following-sibling::ul[1]")
-    IN_WORK_ORDER_ITEMS = (By.XPATH, "//p[text()='В работе:']/following-sibling::ul[1]/li")
+    # Счетчик "Выполнено за все время"
+    TOTAL_ORDERS_COUNTER = (By.XPATH, ".//p[text()='Выполнено за все время:']/following-sibling::p[contains(@class, 'OrderFeed_number')]")
+
+    # Счетчик "Выполнено за сегодня"
+    TODAY_ORDERS_COUNTER = (By.XPATH, ".//p[text()='Выполнено за сегодня:']/following-sibling::p[contains(@class, 'OrderFeed_number')]")
+
+    # Список заказов "В работе" (In progress)
+    ORDERS_IN_PROGRESS_LIST = (By.XPATH, ".//ul[contains(@class, 'OrderFeed_orderListReady')]/preceding-sibling::ul[contains(@class, 'OrderFeed_orderList')]")
+    # Более точный путь для номеров в колонке "В работе" 
+    READY_ORDERS_LIST = (By.XPATH, ".//ul[contains(@class, 'OrderFeed_orderListReady')]")
+
+    PAGE_HEADER = (By.XPATH, "//h1[text()='Лента заказов']")
+    IN_WORK_ORDER_ITEMS = (By.XPATH, "//ul[not(contains(@class, 'orderListReady'))]/li[contains(@class, 'text_type_digits-default')]")
