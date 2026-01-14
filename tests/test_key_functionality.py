109b0c7e46226c93af137c6769198e019ffbbadf
Olga Korzinina <korzininao@gmail.com>
2026-01-14 23:19:31 +0100

    Обновление test_key_functionality

--- tests/test_key_functionality.py
+++ tests/test_key_functionality.py
@@ -3,7 +3,7 @@ from pages.main_page import MainPage
 
 
 class TestKeyFunctionality:
-    
+
     @allure.title("Переход по клику на «Конструктор»")
     @allure.severity(allure.severity_level.CRITICAL)
     def test_navigation_to_constructor(self, driver):
@@ -14,7 +14,6 @@ class TestKeyFunctionality:
         button_text = main_page.get_constructor_button_text()
         assert "Конструктор" in button_text
 
-
     @allure.title("Переход по клику на раздел «Лента заказов»")
     @allure.severity(allure.severity_level.CRITICAL)
     def test_navigation_to_order_feed(self, driver):
@@ -23,8 +22,7 @@ class TestKeyFunctionality:
         main_page.click_order_feed()
         button_text = main_page.get_order_feed_button_text()
         assert "Лента" in button_text
-    
-     
+
     @allure.title("Клик на ингредиент открывает всплывающее окно с деталями")
     @allure.severity(allure.severity_level.NORMAL)
     def test_ingredient_modal_open(self, driver):
@@ -32,39 +30,32 @@ class TestKeyFunctionality:
         main_page.open()
         main_page.click_ingredient()
         modal_visible = main_page.is_modal_visible()
-        assert modal_visible
-    
-    
+        assert modal_visible is True
+
     @allure.title("Всплывающее окно закрывается кликом по крестику")
     @allure.severity(allure.severity_level.NORMAL)
-    def test_ingredient_modal_close_with_main_button(self, driver):
+    def test_ingredient_modal_close(self, driver):
         main_page = MainPage(driver)
         main_page.open()
         main_page.click_ingredient()
-        
-        # Пробуем закрыть модальное окно
         main_page.close_modal()
+        modal_closed = main_page.is_modal_not_displayed()
+        assert modal_closed 
         
-        # Простая проверка - что страница все еще работает
-        # Можно проверить любой элемент на странице
-        button_exists = main_page.get_constructor_button_text()
-        assert button_exists  # Просто проверяем что метод выполнился без ошибок
-
-
     @allure.title("Счетчик ингредиента увеличивается при добавлении в заказ")
     @allure.severity(allure.severity_level.NORMAL)
     def test_ingredient_counter_increases(self, driver):
         main_page = MainPage(driver)
         main_page.open()
-        
-        # Получаем начальное значение счетчика
+
+        # Получаем начальное значение 
         initial_counter = main_page.get_ingredient_counter()
-        
-        # Добавляем ингредиент в конструктор
+
+        # Добавляем ингредиент
         main_page.drag_ingredient_to_constructor()
-        
-        # Получаем новое значение счетчика
+
+        # Получаем новое значение
         new_counter = main_page.get_ingredient_counter()
-        
-        # Проверяем что счетчик изменился
-        assert new_counter != initial_counter
+
+        # Счетчик увеличился ровно на 2 (так как булки добавляются по 2 шт сразу)
+        assert new_counter == initial_counter + 2
